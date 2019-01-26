ae9d346cc3ef810e2e279729064ada66b283823a
Nazar Dizhak <Nazar_Dizhak@epam.com>
2019-01-26 14:58:41 +0200

    Compone Objects

---
+++ JS_Tasks/object_compone.js
@@ -0,0 +1,17 @@
+/**
+ * Compones arrays of objects by id or third param
+ * @param {array} targetArr
+ * @param {array} addedObj
+ * @param {string} componeField
+ */
+function componeObj(targetArr, addedObj, componeField) {
+  let key = componeField ? componeField : 'id';
+  return targetArr.map(el => {
+    let result = Object.assign({}, el);
+    let descript = addedObj.find(descr => el[key] === descr[key]);
+    if (descript) {
+      result.discovery = descript;
+    }
+    return result;
+  });
+}
