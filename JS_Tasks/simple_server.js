794fbd7c2661d5b961e0aede608fea453473ea15
Nazar Dizhak <Nazar_Dizhak@epam.com>
2019-02-12 13:16:35 +0200

    Simple server - add routes; large image download

--- JS_Tasks/simple_server.js
+++ JS_Tasks/simple_server.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 const events = require('./events.json');
+const image = './large_image.jpg';
 const port = 3050;
 
-const server = http.createServer((request, response) => {
-  response.end(JSON.stringify(events));
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/image') {
+    const img = fs.createReadStream(image);
+    img.pipe(res);
+  } else if (url === '/events') {
+    res.end(JSON.stringify(events));
+  }
 });
 
 server.listen(port, (err) => {
