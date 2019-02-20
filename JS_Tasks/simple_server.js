4863d5542c8e3bf4ca147bcefe65b2e11f77c8fa
Nazar Dizhak <Nazar_Dizhak@epam.com>
2019-02-20 12:18:02 +0200

    wip

--- JS_Tasks/simple_server.js
+++ JS_Tasks/simple_server.js
@@ -1,23 +1,69 @@
-const http = require('http');
 const fs = require('fs');
-const events = require('./events.json');
+const eventsPath = './files/events.json';
+const discoveriesPath = './files/discoveries.json';
+const events = require(eventsPath);
+const discoveries = require(discoveriesPath);
 const image = './large_image.jpg';
 const port = 3050;
+const uuid4 = require('uuid/v4');
+const compone = require('./object_compone');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === '/image') {
-    const img = fs.createReadStream(image);
-    img.pipe(res);
-  } else if (url === '/events') {
-    res.end(JSON.stringify(events));
-  }
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/events', function (req, res) {
+  fs.readFile(eventsPath, (err, data) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
 });
 
-server.listen(port, (err) => {
-  if (err) {
-    return 'Error';
-  }
+app.post('/events', function (req, res) {
+  events.push({id: uuid4(), name: req.body.name});
+  fs.writeFile(eventsPath, JSON.stringify(events), err => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(events);
+    }
+  });
+});
 
-  console.log('listening');
-})
+app.get('/events/:id', function (req, res) {
+  let elem = events.find(event => event.id === req.params.id);
+  res.json(elem);
+});
+
+app.get('/discoveries', function (req, res) {
+  fs.readFile(discoveriesPath, (err, data) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  })
+});
+
+app.get('/componed', function (req, res) {
+  res.json(compone(events, discoveries));
+});
+
+app.get('/image', function (req, res) {
+  const img = fs.createReadStream(image);
+  img.pipe(res);
+});
+
+app.get('*', function (req, res) {
+  res.send(`Fuck you ${uuid4()}`);
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
